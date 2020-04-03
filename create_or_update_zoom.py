53518803813124c247c1c782d9661b98e036f67c
Lee Campbell <lee@salk.edu>
2020-04-03 11:56:36 -0700

    fix problem adding panelists to a new webinar

--- create_or_update_zoom.py
+++ create_or_update_zoom.py
@@ -21,6 +21,7 @@ NOW = datetime.datetime.now()
 
 integrations_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(integrations_dir, "zoomus"))
+sys.path.append(integrations_dir)
 import copy
 
 timeformat = "%Y-%m-%dT%H:%M:00Z"   # the python datetime object should be in UTC time.
@@ -92,9 +93,13 @@ existing_checked = False
 
 def get_existing_meetings():
 
+    """leave two variables in the global namespace of this file: existing_zoom_events and existing_checked. Look up
+    existing events here rather than hitting the API for each event.  existing_checked indicates whether existing_zoom_evnets
+    has already been loaded"""
+
     global existing_zoom_events
     global existing_checked
-    from .secrets import client, user_id
+    from secrets import client, user_id
 
 
     existing_meetings_result = client.meeting.list(user_id=user_id, page_size=300)
@@ -127,11 +132,12 @@ def get_existing_meetings():
 
 def create_or_update_zoom(excel_data):
 
-    """Handle starttime and endtime as strings or naive datetime objects depending on how the user typed them in"""
+    """Handle starttime and endtime as strings or naive datetime objects depending on how the user typed them into
+    Excel. If this function is imported into django/python, it should send a timezone aware datetime object. """
 
     global existing_zoom_events
     global existing_checked
-    from .secrets import client, user_id, meeting_defaults, webinar_defaults
+    from secrets import client, user_id, meeting_defaults, webinar_defaults
 
 
     if not existing_checked:
@@ -143,13 +149,12 @@ def create_or_update_zoom(excel_data):
     endtime = excel_data.get("endtime")
     
     if starttime and endtime:
-        
         if type(starttime) == str:
             starttime = datetime.datetime.strptime(starttime, "%Y-%m-%dT%H:%M:00Z")
             endtime = datetime.datetime.strptime(endtime, "%Y-%m-%dT%H:%M:00Z")
 
         elif type(starttime) == datetime.datetime:
-            if datetime.datetime.tzinfo == None:
+            if starttime.tzinfo == None:
 
                 """Excel has no concept of timezones. The timezone is in a column with header 'timezone'.
                 If a timezone is found in the datetime.datetime, then honor it. Otherwise, looking for a 
@@ -167,7 +172,6 @@ def create_or_update_zoom(excel_data):
                 endtime = TZ.localize(endtime)
             else:
                 TZ = starttime.tzinfo
-        
         
         utc_starttime = UTC.normalize(starttime).strftime(timeformat)
         utc_endtime = UTC.normalize(endtime).strftime(timeformat)
@@ -217,6 +221,10 @@ def create_or_update_zoom(excel_data):
                 existing_zoom_events.update({return_val.get("agenda"): return_val})
 
             if meeting_type == "webinar":
+                existing_zoom_event = return_val  #I lookup the meeting ID from existing_zoom_event below.
+
+                """If this is a webinar, add the authors of the excel event as panelists"""
+
                 pheaders = ['name', 'email']
                 panelistStr = excel_data.get("panelists")
                 panelist_list = [parseaddr(item) for item in panelistStr.split(",")]
@@ -226,7 +234,10 @@ def create_or_update_zoom(excel_data):
                 panelist_data = {
                     "panelists": plist
                 }
-                result = client.webinar.add_panelists(user_id=user_id, id=existing_zoom_event.get('id'), **panelist_data)
+                try:
+                    result = client.webinar.add_panelists(user_id=user_id, id=existing_zoom_event.get('id'), **panelist_data)
+                except:
+                    debug()
                 if not result.ok:
                     print(json.loads(response.content))
                 """Remove panelists that are not listed in the source (excel) data"""
