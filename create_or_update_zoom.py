04772062f8d0e0b4bcffc528cead35ade4a1bfbf
Lee Campbell <lee@salk.edu>
2020-04-28 15:47:52 -0700

    stopping for each change.

--- create_or_update_zoom.py
+++ create_or_update_zoom.py
@@ -410,7 +410,7 @@ def create_or_update_zoom(excel_data):
                 zoom_data['settings'].update({"alternative_hosts": alternative_hosts})
 
             zoom_data['recurrence'].update({"endtime": utc_endtime})
-
+            debug()
             if existing_zoom_event:
                 function_call = eval("client.{0}.{1}".format(meeting_type, action))
                 result = function_call(user_id=user_id, id=existing_zoom_event.get("id"), **zoom_data)
